798768e00c8a4a5ede20e0ff6f6938cc99759b53
Reuben-Thorpe <themojotribes@gmail.com>
2016-03-13 18:33:18 +0000

    added odd.numbers.c

---
+++ easy/odd_numbers/odd.numbers.cpp
@@ -0,0 +1,15 @@
+// Reuben Thorpe (2016), CodeEval [Odd Numbers v1.1]
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+  for (int i = 1; i < 100; i++) {
+    if (i % 2 != 0) {
+      cout << i << '\n';
+    }
+
+  }
+  return(0);
+}
